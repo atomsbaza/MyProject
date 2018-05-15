f40995ac24d2d27d37763c60e89c0af06f237efa
geng <pisit-38@live.com>
2018-05-15 12:30:02 +0700

    #3 Update User Method

--- MyProject/Controllers/MemberListsController.cs
+++ MyProject/Controllers/MemberListsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using MyProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -45,25 +47,6 @@ namespace MyProject.Controllers
             return View(users);
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(MemberList memberLists)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Add(memberLists);
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(memberLists);
-        }
-
         // GET: Detail
         public async Task<IActionResult> Detail(string id)
         {
@@ -84,11 +67,77 @@ namespace MyProject.Controllers
         }
 
 
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            ApplicationUser user = await _db.Users.SingleOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
+            return View(user);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", applicationUser);
+            }
+            else
+            {
+                var userInDb = _db.Users.SingleOrDefault(m => m.Id == applicationUser.Id);
+                if (userInDb == null)
+                {
+                    return NotFound();
+                }
 
+                //_db.Users.Update(applicationUser);
+                userInDb.FirstName = applicationUser.FirstName;
+                userInDb.LastName = applicationUser.LastName;
+                userInDb.Email = applicationUser.Email;
+                userInDb.Address = applicationUser.Address;
+                userInDb.City = applicationUser.City;
+                userInDb.PostalCode = applicationUser.PostalCode;
+                userInDb.PhoneNumber = applicationUser.PhoneNumber;
+               
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+        }
 
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser user = await _db.Users.SingleOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveCustomer(string id)
+        {
+            var userinDb = await _db.Users.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Users.Remove(userinDb);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
         protected override void Dispose(bool disposing)
         {
--- MyProject/Utility/StaticDetail.cs
+++ MyProject/Utility/StaticDetail.cs
@@ -8,6 +8,8 @@ namespace MyProject.Utility
     public class StaticDetail
     {
         public const string AdminEndUser = "Admin";
-        public const string CustomerEndUser = "Customer";        
+        public const string CustomerEndUser = "Customer";
+        public const string Active = "Active";
+        public const string NotActive = "Not Active";
     }
 }
